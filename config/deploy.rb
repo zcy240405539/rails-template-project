fccaf6a8c51ecdcc139fc989f3ecae96bfd3723b
zcy240405539 <zcy240405539@gmail.com>
2019-05-20 12:36:07 -0500

    first commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "myapp"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:zcy240405539/rails-template-project.git"
 set :deploy_to, "/home/rails/#{fetch :application}"
 
 # Default branch is :master
@@ -25,7 +25,7 @@ set :deploy_to, "/home/rails/#{fetch :application}"
 # append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets","vendor/bundle", ".bundle", "public/system", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -34,7 +34,7 @@ set :deploy_to, "/home/rails/#{fetch :application}"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
